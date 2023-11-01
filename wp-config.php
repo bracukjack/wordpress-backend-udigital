c95bc5ca7a6016acea54108684f9c50989077ae4
widi <gede.widyadnyana@taksu.tech>
2023-11-01 23:40:51 +0800

    fix bugs

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress-backend-test' );
+define( 'DB_NAME', 'wordpress-developer-test' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
---
+++ wp-content/plugins/gutenpride/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '2d45e0481a75e4895b3d');
